0692eaad6deb8e903770ac908d3c42372392c982
yusshu <yushu@unnamed.team>
2021-09-26 01:56:45 -0500

    fix: implement IdentifierExpression missing methods, add documentation

--- src/main/java/team/unnamed/molang/ast/IdentifierExpression.java
+++ src/main/java/team/unnamed/molang/ast/IdentifierExpression.java
@@ -7,8 +7,12 @@ import team.unnamed.molang.context.EvalContext;
 
 import java.util.List;
 
-public class IdentifierExpression
-        implements Expression {
+/**
+ * Expression implementation for MoLang 1.17
+ * identifiers, they are <b>case-insensitive</b>
+ * See https://bedrock.dev/docs/1.17.0.0/1.17.30.4/Molang#Keywords
+ */
+public class IdentifierExpression implements Expression {
 
     private final String identifier;
 
@@ -21,6 +25,14 @@ public class IdentifierExpression
         return identifier;
     }
 
+    /**
+     * Finds the value from the given {@code context}
+     * environment
+     * @param context The environment to check the
+     *                identifier from
+     * @return The identifier value, or null if not
+     * found
+     */
     @Override
     public Object eval(EvalContext context) {
         Object binding = context.getBinding(identifier);
@@ -56,8 +68,26 @@ public class IdentifierExpression
     }
 
     @Override
-    public String toString() {
+    public String toSource() {
         return identifier;
+    }
+
+    @Override
+    public String toString() {
+        return "Identifier(" + identifier + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        IdentifierExpression that = (IdentifierExpression) o;
+        return identifier.equals(that.identifier);
+    }
+
+    @Override
+    public int hashCode() {
+        return identifier.hashCode();
     }
 
 }
