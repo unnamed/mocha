58ff238ec76cc40be36e929ced7204acb7ebe5f1
yusshu <yushu@unnamed.team>
2021-09-26 01:51:29 -0500

    fix: make ExecutionScope use the scope return value

--- src/main/java/team/unnamed/molang/ast/ExecutionScopeExpression.java
+++ src/main/java/team/unnamed/molang/ast/ExecutionScopeExpression.java
@@ -4,7 +4,14 @@ import team.unnamed.molang.context.EvalContext;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 
+/**
+ * Expression implementation of MoLang 1.17
+ * execution scopes
+ * See https://bedrock.dev/docs/1.17.0.0/1.17.30.4/
+ * Molang#%7B%20%7D%20Brace%20Scope%20Delimiters
+ */
 public class ExecutionScopeExpression implements Expression {
 
     private final List<Expression> expressions;
@@ -13,30 +20,60 @@ public class ExecutionScopeExpression implements Expression {
         this.expressions = expressions;
     }
 
+    /**
+     * Returns the expressions inside this
+     * execution scope, never null
+     */
     public List<Expression> getExpressions() {
         return expressions;
     }
 
     @Override
     public Object eval(EvalContext context) {
-        Object last = 0;
         for (Expression expression : expressions) {
-            last = expression.eval(context);
+            // eval expression, ignore result
+            // todo: we should use statements!
+            expression.eval(context);
+
+            // check for return values
+            Object returnValue = context.popReturnValue();
+            if (returnValue != null) {
+                return returnValue;
+            }
         }
-        return last;
+        return 0;
     }
 
     @Override
-    public String toString() {
+    public String toSource() {
         StringBuilder builder = new StringBuilder("{");
         Iterator<Expression> iterator = expressions.iterator();
         while (iterator.hasNext()) {
             Expression expression = iterator.next();
-            builder.append(expression.toString());
+            builder.append(expression.toSource());
             if (iterator.hasNext()) {
                 builder.append("; ");
             }
         }
         return builder.append('}').toString();
     }
+
+    @Override
+    public String toString() {
+        return "ExecutionScope(" + this.expressions + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ExecutionScopeExpression that = (ExecutionScopeExpression) o;
+        return expressions.equals(that.expressions);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(expressions);
+    }
+
 }
