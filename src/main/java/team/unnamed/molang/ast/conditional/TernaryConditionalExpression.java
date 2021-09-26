36c27ec5ad8d726a9f59618f02b645ed769fe9b9
yusshu <yushu@unnamed.team>
2021-09-26 02:04:30 -0500

    fix: missing methods in TernaryConditionalExpression, add docs

--- src/main/java/team/unnamed/molang/ast/conditional/TernaryConditionalExpression.java
+++ src/main/java/team/unnamed/molang/ast/conditional/TernaryConditionalExpression.java
@@ -3,9 +3,13 @@ package team.unnamed.molang.ast.conditional;
 import team.unnamed.molang.context.EvalContext;
 import team.unnamed.molang.ast.Expression;
 
+import java.util.Objects;
+
 /**
- * Implementation of the ternary conditional expression,
+ * Implementation of MoLang 1.17 ternary conditional expression,
  * it's similar to an "if {...} else {...}" expression.
+ *
+ * See https://bedrock.dev/docs/1.17.0.0/1.17.30.4/Molang#Conditionals
  *
  * Depending on the conditional expression, it may
  * return the {@code trueExpression} or the {@code falseExpression}
@@ -26,6 +30,31 @@ public class TernaryConditionalExpression implements Expression {
         this.falseExpression = falseExpression;
     }
 
+    /**
+     * Returns the condition of this ternary conditional expression, if this
+     * condition is evaluated to {@code true}, the {@code trueExpression} is
+     * evaluated and returned as value when calling {@link Expression#eval}
+     */
+    public Expression getConditional() {
+        return conditional;
+    }
+
+    /**
+     * Returns the expression evaluated when the {@code conditional} expression
+     * is evaluated to {@code true}
+     */
+    public Expression getTrueExpression() {
+        return trueExpression;
+    }
+
+    /**
+     * Returns the expression evaluated when the {@code conditional} expression
+     * is evaluated to {@code false}
+     */
+    public Expression getFalseExpression() {
+        return falseExpression;
+    }
+
     @Override
     public Object eval(EvalContext context) {
         return conditional.evalAsBoolean(context)
@@ -34,10 +63,32 @@ public class TernaryConditionalExpression implements Expression {
     }
 
     @Override
+    public String toSource() {
+        return conditional.toSource()
+                + " ? " + trueExpression.toSource()
+                + " : " + falseExpression.toSource();
+    }
+
+    @Override
     public String toString() {
-        return conditional
-                + " ? " + trueExpression
-                + " : " + falseExpression;
+        return "TernaryCondition(" + conditional + ", "
+                + trueExpression + ", "
+                + falseExpression + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TernaryConditionalExpression that = (TernaryConditionalExpression) o;
+        return conditional.equals(that.conditional)
+                && trueExpression.equals(that.trueExpression)
+                && falseExpression.equals(that.falseExpression);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(conditional, trueExpression, falseExpression);
     }
 
 }
