ba4541fd4813d30b44d851df8bd3fda41c2d8784
yusshu <yushu@unnamed.team>
2021-11-01 00:23:33 -0500

    fix: assertion order, first parameter is the expected value

--- src/test/java/team/unnamed/molang/MoLangAssertions.java
+++ src/test/java/team/unnamed/molang/MoLangAssertions.java
@@ -9,12 +9,12 @@ public class MoLangAssertions {
 
     private static final ScriptEngine ENGINE = new MoLangScriptEngineFactory().getScriptEngine();
 
-    public static void assertEquals(float value, String... expressions) {
+    public static void assertEquals(float expected, String... expressions) {
         String expression = String.join(";", expressions);
         try {
             Object result = ENGINE.eval(expression);
             Assertions.assertTrue(result instanceof Number, "Result is a number");
-            Assertions.assertEquals(((Number) result).floatValue(), value);
+            Assertions.assertEquals(expected, ((Number) result).floatValue());
         } catch (ScriptException e) {
             Assertions.fail("Failed to eval expression: " + expression, e);
         }
